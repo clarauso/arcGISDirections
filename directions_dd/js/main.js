31c9d21f77aaabd20fd14a3738a5f112eb5c6ddd
Claudio Rauso <clarauso@gmail.com>
2013-12-31 17:24:06 +0100

    move start with timer

--- directions_dd/js/main.js
+++ directions_dd/js/main.js
@@ -2,9 +2,6 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 	return declare("", null, {
 		config : {},
 		constructor : function(config) {
-			//config will contain application and user defined info for the template such as i18n strings, the web map id
-			// and application id
-			// any url parameters and any application specific configuration information.
 			this.config = config;
 			ready(lang.hitch(this, function() {
 				this._createWebMap();
@@ -13,11 +10,6 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 		_mapLoaded : function() {
 			// Map is ready
 			console.log("mapLoaded");
-			// directions service
-			var directions = new Directions({
-				map : this.map
-			}, "dir");
-			directions.startup();
 			var map = this.map;
 			var toRemove;
 			var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
@@ -34,11 +26,22 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 			routeParameters.outSpatialReference = {
 				"wkid" : 102100
 			};
-			var skip = 10;
+			var task;
 			stops = new GraphicsLayer();
 			stops.on("mouse-down", function(evt) {
-				this.getMap().disablePan();
 				console.log("down");
+				task = setInterval(function() {
+					var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+					routeTask.on("solve-complete", function(evt) {
+						if (!(toRemove === undefined))
+							map.graphics.remove(toRemove);
+						toRemove = map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
+					});
+					routeTask.on("error", function(evt) {
+						console.log("routeTask error");
+					});
+					routeTask.solve(routeParameters);
+				}, 600);
 			});
 			stops.on("mouse-drag", function(evt) {
 				routeParameters.stops.features.splice(0, 1);
@@ -46,21 +49,9 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 				this.remove(evt.graphic);
 			});
 			stops.on("mouse-up", function(evt) {
-				this.getMap().enablePan();
-				// temporizzare quanto segue
-				var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
-				routeTask.on("solve-complete", function(evt) {
-					if (!(toRemove === undefined))
-						map.graphics.remove(toRemove);
-					toRemove = map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
-				});
-				routeTask.on("error", function(evt) {
-					console.log("routeTask error");
-				});
-				routeTask.solve(routeParameters);
+				clearInterval(task);
 			});
 			this.map.addLayer(stops);
-
 			symbol = new SimpleMarkerSymbol();
 			symbol.setStyle(SimpleMarkerSymbol.STYLE_DIAMOND).setSize(15);
 			routeSymbol = new SimpleLineSymbol();
@@ -103,17 +94,11 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 		_createWebMap : function() {
 			arcgisUtils.createMap(this.config.webmap, "map", {
 				mapOptions : {
-					//Optionally define additional map config here for example you can
-					//turn the slider off, display info windows, disable wraparound 180, slider position and more.
 					center : [12.483162, 41.8964],
 					zoom : 13
 				},
 				bingMapsKey : this.config.bingmapskey
 			}).then(lang.hitch(this, function(response) {
-				//Once the map is created we get access to the response which provides important info
-				//such as the map, operational layers, popup info and more. This object will also contain
-				//any custom options you defined for the template. In this example that is the 'theme' property.
-				//Here' we'll use it to update the application to match the specified color theme.
 				this.map = response.map;
 				if (this.map.loaded) {
 					this._mapLoaded();
@@ -123,10 +108,6 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color
 					}));
 				}
 			}), lang.hitch(this, function(error) {
-				//an error occurred - notify the user. In this example we pull the string from the
-				//resource.js file located in the nls folder because we've set the application up
-				//for localization. If you don't need to support mulitple languages you can hardcode the
-				//strings here and comment out the call in index.html to get the localization strings.
 				if (this.config && this.config.i18n) {
 					alert(this.config.i18n.map.error + ": " + error.message);
 				} else {
