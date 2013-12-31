18ca7de19083a9b23f05bd970c642a84af05e748
Claudio Rauso <clarauso@gmail.com>
2013-12-31 16:31:04 +0100

    RouteTask usage

--- directions_dd/js/main.js
+++ directions_dd/js/main.js
@@ -1,4 +1,4 @@
-define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "esri/arcgis/utils", "esri/IdentityManager", "dojo/on", "esri/dijit/Directions", "esri/tasks/locator", "esri/geometry/webMercatorUtils"], function(ready, declare, lang, arcgisUtils, IdentityManager, on, Directions, Locator, webMercatorUtils) {
+define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color", "esri/arcgis/utils", "esri/IdentityManager", "dojo/on", "esri/dijit/Directions", "esri/tasks/locator", "esri/geometry/webMercatorUtils", "esri/layers/GraphicsLayer", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol", "esri/graphic", "esri/tasks/RouteTask", "esri/tasks/RouteParameters", "esri/tasks/FeatureSet"], function(ready, declare, lang, Color, arcgisUtils, IdentityManager, on, Directions, Locator, webMercatorUtils, GraphicsLayer, SimpleMarkerSymbol, SimpleLineSymbol, Graphic, RouteTask, RouteParameters, FeatureSet) {
 	return declare("", null, {
 		config : {},
 		constructor : function(config) {
@@ -18,8 +18,67 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "esri/arcgis/util
 				map : this.map
 			}, "dir");
 			directions.startup();
-			this.map.on("click", function(evt) {
-				console.log(locator.locationToAddress(webMercatorUtils.webMercatorToGeographic(evt.mapPoint), 100));
+			var map = this.map;
+			var toRemove;
+			var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+			routeTask.on("solve-complete", function(evt) {
+				if (!(toRemove === undefined))
+					map.graphics.remove(toRemove);
+				toRemove = map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
+			});
+			routeTask.on("error", function(evt) {
+				console.log("routeTask error");
+			});
+			routeParameters = new RouteParameters();
+			routeParameters.stops = new FeatureSet();
+			routeParameters.outSpatialReference = {
+				"wkid" : 102100
+			};
+			var skip = 10;
+			stops = new GraphicsLayer();
+			stops.on("mouse-down", function(evt) {
+				this.getMap().disablePan();
+				console.log("down");
+			});
+			stops.on("mouse-drag", function(evt) {
+				routeParameters.stops.features.splice(0, 1);
+				routeParameters.stops.features.splice(0, 0, this.add(new Graphic(evt.mapPoint, symbol)));
+				this.remove(evt.graphic);
+			});
+			stops.on("mouse-up", function(evt) {
+				this.getMap().enablePan();
+				// temporizzare quanto segue
+				var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+				routeTask.on("solve-complete", function(evt) {
+					if (!(toRemove === undefined))
+						map.graphics.remove(toRemove);
+					toRemove = map.graphics.add(evt.result.routeResults[0].route.setSymbol(routeSymbol));
+				});
+				routeTask.on("error", function(evt) {
+					console.log("routeTask error");
+				});
+				routeTask.solve(routeParameters);
+			});
+			this.map.addLayer(stops);
+
+			symbol = new SimpleMarkerSymbol();
+			symbol.setStyle(SimpleMarkerSymbol.STYLE_DIAMOND).setSize(15);
+			routeSymbol = new SimpleLineSymbol();
+			routeSymbol.setColor(new Color([0, 0, 255, 0.5]));
+			routeSymbol.setWidth(5);
+			/*
+			 this.map.on("dbl-click", function(evt) {
+			 locator.locationToAddress(webMercatorUtils.webMercatorToGeographic(evt.mapPoint), 100);
+			 });
+			 */
+			this.map.on("dbl-click", function(evt) {
+				//get the associated node info when the graphic is clicked
+				var g = new Graphic(evt.mapPoint, symbol);
+				if (routeParameters.stops.features.length < 2)
+					routeParameters.stops.features.push(this.getLayer("graphicsLayer0").add(g));
+				if (routeParameters.stops.features.length == 2) {
+					routeTask.solve(routeParameters);
+				}
 			});
 			// reverse geocoding service
 			var locator = new Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
@@ -29,19 +88,15 @@ define(["dojo/ready", "dojo/_base/declare", "dojo/_base/lang", "esri/arcgis/util
 						directions.updateStop(evt.address.address.Address + ", " + evt.address.address.City, 0);
 					} else {
 						directions.updateStop(evt.address.address.Address + ", " + evt.address.address.City, 1).then(function(value) {
-							// directions updated, get directions 
+							// directions updated, get directions
 							directions.emit("get-directions", null);
 						}, function(err) {
 							// Do something when the process errors out
 						}, function(update) {
 							// Do something when the process provides progress information
 						});
-						;
 					}
 				}
-			});
-			directions.on("get-directions", function(evt) {
-				directions.getDirections();
 			});
 		},
 		//create a map based on the input web map id
