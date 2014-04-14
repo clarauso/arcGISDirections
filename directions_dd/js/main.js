bbee66a64dcfda5aeb80c02bad4cc89e1a3e4ce4
Claudio Rauso <clarauso@gmail.com>
2014-04-14 18:15:00 +0200

    DirectionsLocator

--- directions_dd/js/main.js
+++ directions_dd/js/main.js
@@ -1,12 +1,12 @@
 define(["dojo/ready", "dojo/_base/array", "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Color", "esri/arcgis/utils", "esri/IdentityManager", "dojo/on", 
-"esri/tasks/locator", "esri/geometry/webMercatorUtils", "esri/layers/GraphicsLayer", "esri/symbols/SimpleLineSymbol", "esri/graphic", "esri/tasks/RouteTask", 
+"esri/geometry/webMercatorUtils", "esri/layers/GraphicsLayer", "esri/symbols/SimpleLineSymbol", "esri/graphic", "esri/tasks/RouteTask", 
 "esri/tasks/RouteParameters", "esri/tasks/FeatureSet", "esri/geometry/Point", "application/utils/DirectionsMenu", "application/utils/DirectionsMenuItem", 
-"application/utils/DirectionsEdit", "dojo/query", "dojo/keys", "dijit/registry", "application/utils/StopManager", "dgrid/Grid", "dojo/number", "dojo/dom-construct", 
+"application/utils/DirectionsEdit", "application/utils/DirectionsLocator", "dojo/query", "dojo/keys", "dijit/registry", "application/utils/StopManager", "dgrid/Grid", "dojo/number", "dojo/dom-construct", 
 "esri/lang", "esri/units", "dijit/form/Button", "dojo/promise/all"], 
 function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, on,
-	Locator, webMercatorUtils, GraphicsLayer, SimpleLineSymbol, Graphic, RouteTask, 
+	webMercatorUtils, GraphicsLayer, SimpleLineSymbol, Graphic, RouteTask, 
 	RouteParameters, FeatureSet, Point, DirectionsMenu, DirectionsMenuItem, 
-	DirectionsEdit, query, keys, registry, StopManager, Grid, number, domConstruct,
+	DirectionsEdit, DirectionsLocator, query, keys, registry, StopManager, Grid, number, domConstruct,
 	esriLang, esriUnits, Button, all) {
 	return declare("", null, {
 		config : {},
@@ -90,54 +90,15 @@ function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, 
 				console.log("routeTask error");
 			});
 			var routeTask = this.routeTask;
-			var startLocator = new Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+			// locators for geocoding
+			var startLocator = new DirectionsLocator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
 			startLocator.outSpatialReference = map.spatialReference;
-			startLocator.on("address-to-locations-complete", function(evt) {
-				stopManager.addStop(0, evt.addresses[0].location, false);
-				map.getLayer("graphicsLayer0").graphics[0].setAttributes({
-					name : evt.addresses[0].address
-				});
-			});
-			startLocator.on("location-to-address-complete", function(evt) {
-				if (evt.address.address) {
-					// move point according to reverse geocode
-					map.getLayer("graphicsLayer0").graphics[0].setGeometry(evt.address.location);
-					map.getLayer("graphicsLayer0").graphics[0].setAttributes({
-						name : evt.address.address.Address + ", " + evt.address.address.City
-					});
-					registry.byId("start").set("value", evt.address.address.Address + ", " + evt.address.address.City);
-					routeParameters.stops.features.splice(0, 1, map.getLayer("graphicsLayer0").graphics[0]);
-					if (map.getLayer("graphicsLayer0").graphics[0] != null && map.getLayer("graphicsLayer0").graphics[1] != null) {
-						routeParameters.returnDirections = true;
-						routeParameters.returnStops = true;
-						routeTask.solve(routeParameters);
-					}
-				}
-			});
-			var endLocator = new Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+			startLocator.setIndex(0);
+			startLocator.setMain(this);
+			var endLocator = new DirectionsLocator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
 			endLocator.outSpatialReference = map.spatialReference;
-			endLocator.on("address-to-locations-complete", function(evt) {
-				stopManager.addStop(1, evt.addresses[0].location, false);
-				map.getLayer("graphicsLayer0").graphics[1].setAttributes({
-					name : evt.addresses[0].address
-				});
-			});
-			endLocator.on("location-to-address-complete", function(evt) {
-				if (evt.address.address) {
-					// move point according to reverse geocode
-					map.getLayer("graphicsLayer0").graphics[1].setGeometry(evt.address.location);
-					map.getLayer("graphicsLayer0").graphics[1].setAttributes({
-						name : evt.address.address.Address + ", " + evt.address.address.City
-					});
-					registry.byId("end").set("value", evt.address.address.Address + ", " + evt.address.address.City);
-					routeParameters.stops.features.splice(1, 1, map.getLayer("graphicsLayer0").graphics[1]);
-					if (map.getLayer("graphicsLayer0").graphics[0] != null && map.getLayer("graphicsLayer0").graphics[1] != null) {
-						routeParameters.returnDirections = true;
-						routeParameters.returnStops = true;
-						routeTask.solve(routeParameters);
-					}
-				}
-			});
+			endLocator.setIndex(1);
+			endLocator.setMain(this);
 			// move start point
 			var startEdit = new DirectionsEdit(this.map);
 			startEdit.setMain(this);
@@ -146,11 +107,10 @@ function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, 
 			var endEdit = new DirectionsEdit(this.map);
 			endEdit.setMain(this);
 			endEdit.setLocator(endLocator);
-			var stopManager = new StopManager(map, routeParameters, [startLocator, endLocator], new Array(startEdit, endEdit));
-			// context menu
+			var stopManager = new StopManager(map, routeParameters, [startLocator, endLocator], [startEdit, endEdit]);
+			// context menu with items
 			var dirMenu = new DirectionsMenu();
 			dirMenu.setMap(this.map);
-			// start
 			var startItem = new DirectionsMenuItem({
 				label : "Parti da qui"
 			});
@@ -158,7 +118,6 @@ function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, 
 			startItem.setMenu(dirMenu);
 			startItem.setStopManager(stopManager);
 			dirMenu.addChild(startItem);
-			// end
 			var endItem = new DirectionsMenuItem({
 				label : "Arriva qui"
 			});
@@ -169,6 +128,7 @@ function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, 
 			// activate context menu
 			dirMenu.startup();
 			dirMenu.bindDomNode(map.container);
+			// solve route button
 			var routeButton = new Button({
 				onClick : function() {
 					all({
@@ -189,6 +149,9 @@ function(ready, arrayUtils, declare, lang, Color, arcgisUtils, IdentityManager, 
 					});
 				}
 			}, "routeButton");
+		},
+		setStopField : function(index, string) {
+			registry.byId((index == 0 ? "start" : "end")).set("value", string);
 		},
 		//create a map based on the input web map id
 		_createWebMap : function() {
